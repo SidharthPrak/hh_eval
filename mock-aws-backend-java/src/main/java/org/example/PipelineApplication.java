336dc12353c18e8b5564c4c31d7ce972acb9696f
SidharthPrak <sx2236@nyu.edu>
2023-12-09 12:25:10 -0500

    Files for kubernetes

--- mock-aws-backend-java/src/main/java/org/example/PipelineApplication.java
+++ mock-aws-backend-java/src/main/java/org/example/PipelineApplication.java
@@ -2,8 +2,10 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+//@EnableKafka
 public class SampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(SampleApplication.class, args);
